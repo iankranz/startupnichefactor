1e8bc4951763b2ce41b73f9bc4f462a58659acbd
iankranz <ian@iankranz.com>
2023-11-15 17:27:50 -0500

    add new scores to serializer

--- list/serializers.py
+++ list/serializers.py
@@ -4,4 +4,4 @@ from .models import Startup
 class StartupSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Startup
-        fields = ('id', 'name', 'tagline', 'summary', 'location', 'niche_score', 'website_url', 'linkedin_url', 'created_at')
+        fields = ('id', 'name', 'tagline', 'summary', 'location', 'niche_score', 'website_url', 'linkedin_url', 'created_at', 'fire_score', 'snooze_score')
